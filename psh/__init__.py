3684540861a5fd4ed238cd79de9818c4d241c26f
Andrey Elizarov <vigilancer@gmail.com>
2018-06-05 03:38:16 +0400

    remove todo and examples from code

--- psh/__init__.py
+++ psh/__init__.py
@@ -1,35 +1,8 @@
 # vim: set ts=2 sw=2 st=2 et:
-
 name = 'psh'
 
-# TODO:
-# - create github repo
-# - exec with subprocess
-# - redirect > to file and /dev/null
-# - redirect >> to file and /dev/null
-# - handle commands with params
-# - stub not implemented operators
-# - override truth (__bool__) based on return code (nonzero == False)
-# - implement 'pipefail' somehow.
-#       maybe with `|=` (__ior__)
-#       _('ls') | _('grep') |= ('xargs')
-#       if `grep` fails, whole chain will fail and xargs will not be executed
-# - maybe pipe by default should work in `pipefail` mode, and __ior__ override it
-# - redirect stdout and stderror
-# - write tests
-# - create package to install with pip
-# - emulate `set -e` and/or `set -o pipefail`
-#
-# Links:
-# https://docs.python.org/3.6/library/operator.html
-# https://docs.python.org/3/library/subprocess.html
-#
-# Examples:
-# pattern = 'Pip'
-# (_('ls') | _(f'grep {pattern}')).stdout
-
 import shlex
-from subprocess import Popen, PIPE
+from subprocess import PIPE, Popen
 
 
 class AbsCommand():
