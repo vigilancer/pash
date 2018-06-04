0ca1669b90d4bf5066eb2a12477167f13dabd0e7
Andrey Elizarov <vigilancer@gmail.com>
2018-06-05 02:21:48 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.adoc", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pash",
+    version="0.0.1",
+    author="Andrey Elizarov",
+    author_email="vigilancer@example.com",
+    description="shell pipes and redirecting for python",
+    long_description=long_description,
+    long_description_content_type="text/asciidoc",
+    url="https://github.com/vigilancer/pash",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved",
+        "Operating System :: OS Independent",
+    ),
+    license="WTFPL",
+)
