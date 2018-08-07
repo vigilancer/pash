e2f9abb6671eb7e793f1f093eff4f57443fab1e3
Andrey Elizarov <vigilancer@gmail.com>
2018-08-07 19:28:26 +0400

    emperor's new (test) suite

---
+++ tests.new/run_tests.bash
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+function run_single_test() {
+  local test_name="$1"; shift
+  local bash="$1"; shift
+  local python="$1"; shift
+
+  local test_bash="test_$test_name.bash"
+  local test_python="test_$test_name.py"
+
+  echo "-- [ $test_name ] --"
+
+  if [[ -z $VERBOSE_DIFF ]]; then
+    diff -q \
+      <("$bash" "$test_bash") <("$python" "$test_python") >/dev/null 2>&1
+  else
+    diff --side-by-side --expand-tabs \
+      <("$bash" "$test_bash") <("$python" "$test_python")
+  fi
+
+  if [[ $? -eq 0 ]]; then
+    echo OK
+  else
+    echo FAIL
+    echo "To know more:"
+    echo "    VERBOSE_DIFF=1 $(basename $0) $test_name"
+
+  fi
+}
+
+function _print_env() {
+  local bash="$1"; shift
+  local python="$1"; shift
+
+  echo 'Testing env:'
+  echo "Bash: $bash"
+  echo "Python: $python"
+}
+
+function main() {
+  local test_names=(
+    'smoke'
+    'fail'
+  )
+  [[ $# -ne 0 ]] && test_names=($@)
+  echo $#
+
+  local bash_path=$(which bash)
+  local python_path=$(which python3)
+
+  _print_env "$bash_path" "$python_path"
+
+  for test_name in "${test_names[@]}"; do
+    run_single_test "$test_name" \
+      "$bash_path" "$python_path"
+  done
+}
+
+main "$@"
---
+++ tests.new/test_fail.py
@@ -0,0 +1,7 @@
+# vim: set ts=4 sw=4 st=4 et:
+
+# import os; import sys; sys.path.insert(0, os.getcwd())
+import os; import sys; sys.path.insert(0, os.path.abspath('..'))
+from pipeish import Pipe as _
+
+_('echo and this is from python')
